9952a0c8100ed2056d03ad8e677945db208dd646
Kami <josephkiarie561@gmail.com>
2026-01-28 22:13:24 +0300

    equipment schemas

---
+++ app/schemas/equipment.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class EquipmentBase(BaseModel):
+    name: str
+    location: str
+
+class EquipmnentCreate(EquipmentBase):
+    pass
+
+class EquipmentRead(EquipmentBase):
+    id: int
+    installed_at: datetime
+
+    model_config = {
+        "from_attributes": True
+    }
