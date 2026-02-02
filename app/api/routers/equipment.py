542e2bbca691d22a8fc3bb364fbb7ea9d64c405f
Kami <josephkiarie561@gmail.com>
2026-02-02 11:18:27 +0300

    routers initialization

--- app/api/routers/equipment.py
+++ app/api/routers/equipment.py
@@ -2,3 +2,12 @@
 # equipment routers
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+
+from app.db.session import get_db
+from app.schemas.equipment import EquipmnentCreate
+from app.crud import equipment as crud
+
+router = APIRouter(
+    prefix = "/equipment",
+    tags = ["Equipment"]
+)
